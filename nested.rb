7eca6a39b00881e200d77d8d657faac98430e7d4
manon jacquin <manon.jacquin@gmail.com>
2019-02-26 13:06:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -17,7 +17,6 @@ def hopper
      }
      
      programmer_hash[:grace_hopper]
-     programmer_hash
 
 end
 
@@ -40,7 +39,6 @@ def alan_kay_is_known_for
         }
      }
      programmer_hash[:alan_kay][:known_for]
-     programmer_hash
 end
 
 def dennis_ritchies_language
