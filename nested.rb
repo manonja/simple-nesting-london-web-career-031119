b1d27b40953137b5fdd97f5588eaef49990ff45d
manon jacquin <manon.jacquin@gmail.com>
2019-02-26 13:06:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -39,6 +39,7 @@ def alan_kay_is_known_for
         }
      }
      programmer_hash[:alan_kay][:known_for]
+     programmer_hash
 end
 
 def dennis_ritchies_language
@@ -58,6 +59,7 @@ def dennis_ritchies_language
         }
      }
      programmer_hash[:dennis_ritchie][:languages].join('')
+     programmer_hash
 end
 
 def adding_matz
@@ -110,6 +112,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
 end
 
